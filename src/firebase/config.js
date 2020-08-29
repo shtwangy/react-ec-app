d3a56c8b5f5c9248b48fcd3b38a28b83195f193d
shtwangy <shtwangy@gmail.com>
2020-08-29 22:18:55 +0900

    feat(src/firebase): create firebase config.
    Closes #16

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAxX-vEj5u0Qo_w-iPC71uBSpgRtvnlHew",
+    authDomain: "react-ec-app-f0c27.firebaseapp.com",
+    databaseURL: "https://react-ec-app-f0c27.firebaseio.com",
+    projectId: "react-ec-app-f0c27",
+    storageBucket: "react-ec-app-f0c27.appspot.com",
+    messagingSenderId: "184283212222",
+    appId: "1:184283212222:web:ef55f5d365a98b9b4e31ef",
+    measurementId: "G-78XKSWPFFW"
+};
