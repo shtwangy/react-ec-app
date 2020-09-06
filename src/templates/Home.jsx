d1261523d2ad1142418031b6806f48be7fd4a1cb
shtwangy <shtwangy@gmail.com>
2020-09-06 13:36:46 +0900

    feat(src/templates/Home.jsx): add sign out button.
    Closes #2

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import { getUserId, getUsername } from "../reducks/users/selectors";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { singOut } from "../reducks/users/operations";
 
 const Home = () => {
+    const dispatch = useDispatch();
     const selector = useSelector(state => state);
     const uid = getUserId(selector);
     const username = getUsername(selector);
@@ -11,6 +13,7 @@ const Home = () => {
             <h2>Home</h2>
             <p>ID: {uid}</p>
             <p>NAME: {username}</p>
+            <button onClick={ () => dispatch(singOut()) }>SIGN OUT</button>
         </div>
     );
 };
