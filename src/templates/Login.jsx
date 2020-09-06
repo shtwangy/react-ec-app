f44d782aa29ac100524230111e622aa746958855
shtwangy <shtwangy@gmail.com>
2020-09-06 13:22:28 +0900

    chore(src/templates/Login.jsx): remove unnecessary imports.

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import { push } from "connected-react-router";
-import {signInAction} from "../reducks/users/actions";
 import {signIn} from "../reducks/users/operations";
 
 const Login = () => {
