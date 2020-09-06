9cc93c1bbd7e375ed9509fb7ebd5e1f4cb272997
shtwangy <shtwangy@gmail.com>
2020-09-06 14:05:23 +0900

    feat(src/templates/SignIn.jsx): add routing to sign up screen and reset password screen.

--- src/templates/SignIn.jsx
+++ src/templates/SignIn.jsx
@@ -2,6 +2,7 @@ import React, {useCallback, useState} from "react";
 import { TextInput, PrimaryButton } from "../components/UIkit";
 import { signIn } from "../reducks/users/operations";
 import { useDispatch } from "react-redux";
+import { push } from 'connected-react-router';
 
 const SignIn = () => {
     const dispatch = useDispatch();
@@ -47,6 +48,9 @@ const SignIn = () => {
                     label={"Sign In"}
                     onClick={() => dispatch(signIn(email, password))}
                 />
+                <div className="module-spacer--medium" />
+                <p onClick={() => dispatch(push('/signup'))}>アカウントをお持ちでない方はこちら</p>
+                <p onClick={() => dispatch(push('/signin/reset'))}>パスワードを忘れた方はこちら</p>
             </div>
         </div>
     )
