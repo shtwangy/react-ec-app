5fb126b1756e7b1c0e66c94573f06a0f965bb243
shtwangy <shtwangy@gmail.com>
2020-09-09 01:20:05 +0900

    feat(src/reducks/store/store.js): add products reducer.
    Issues #3

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -5,13 +5,15 @@ import {
 } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
+import { ProductsReducer } from "../products/reducers";
 import { UsersReducer } from "../users/reducers";
 
 export default function createStore(history) {
     return reduxCreateStore(
         combineReducers({
             router: connectRouter(history),
-            users: UsersReducer
+            users: UsersReducer,
+            products: ProductsReducer
         }),
         applyMiddleware(
             routerMiddleware(history),
