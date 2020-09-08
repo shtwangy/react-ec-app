192690147fe590806572a8a82d4b659228a67af0
shtwangy <shtwangy@gmail.com>
2020-09-09 01:23:28 +0900

    feat(src/Router.jsx): add routing to the product edit screen.
    Closes #3

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { Home, SignUp, SignIn, Reset } from "./templates";
+import { Home, SignUp, SignIn, Reset, ProductEdit } from "./templates";
 import Auth from "./Auth";
 
 const Router = () => {
@@ -8,9 +8,11 @@ const Router = () => {
         <Switch>
             <Route exact path="/signup" component={SignUp}/>
             <Route exact path="/signin" component={SignIn}/>
-            <Route exact path="/signin/reset" component={Reset} />
+            <Route exact path="/signin/reset" component={Reset}/>
+
             <Auth>
                 <Route exact path="(/)?" component={Home} />
+                <Route exact path="/product/edit" component={ProductEdit}/>
             </Auth>
         </Switch>
     );
