feba7e2608f5d6d97804a970eeea4a94dc65751c
shtwangy <shtwangy@gmail.com>
2020-09-25 02:12:36 +0900

    feat(src/templates/ProductList.jsx): create product list component.
    Closes #4

---
+++ src/templates/ProductList.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react';
+import {ProductCard} from "../components/Products";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchProducts} from "../reducks/products/operations";
+import {getProducts} from "../reducks/products/selectors";
+
+const ProductList = () => {
+    const dispatch = useDispatch();
+    const selector = useSelector((state) => state);
+    const products = getProducts(selector);
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, []);
+    return (
+        <section className="c-section-wrapin">
+            <div className="p-grid__row">
+                {products.length > 0 && (
+                    products.map(product => (
+                        <ProductCard
+                            key={product.id}
+                            id={product.id}
+                            images={product.images}
+                            name={product.name}
+                            price={product.price}
+                        />
+                    ))
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default ProductList;
