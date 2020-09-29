097a87cd61083e0a101ffc5e2eaf04104392b354
shtwangy <shtwangy@gmail.com>
2020-09-30 00:13:06 +0900

    feat(src/Router.jsx): add cart routing.
    Closes #14

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { SignUp, SignIn, Reset, ProductEdit, ProductList, ProductDetail } from "./templates";
+import { SignUp, SignIn, Reset, ProductEdit, ProductList, ProductDetail, CartList } from "./templates";
 import Auth from "./Auth";
 
 const Router = () => {
@@ -14,6 +14,7 @@ const Router = () => {
                 <Route exact path="(/)?" component={ProductList}/>
                 <Route exact path="/product/:id" component={ProductDetail}/>
                 <Route path="/product/edit(/:id)?" component={ProductEdit}/>
+                <Route exact path="/cart" component={CartList}/>
             </Auth>
         </Switch>
     );
