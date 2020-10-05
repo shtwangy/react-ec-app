5c260e9618241d1c89116686eea053efe84823f4
shtwangy <shtwangy@gmail.com>
2020-10-05 22:19:12 +0900

    feat(src/functions/common.js): add convert date functions.
    Issues #9

--- src/functions/common.js
+++ src/functions/common.js
@@ -12,3 +12,28 @@ export const returnCodeToBr = (text) => {
         return HTMLReactParser(text.replace(/\r?\n/g, '<br/>'))
     }
 };
+
+/**
+ * Convert datetime into the String.
+ * @param {Date} dt
+ * @returns {string} "YYYY-MM-DD"
+ */
+export const dateToString = (dt) => {
+    return dt.getFullYear() + '-'
+        + ('00' + (dt.getMonth()+1)).slice(-2) + '-'
+        + ('00' + dt.getDate()).slice(-2)
+};
+
+/**
+ * Convert datetime into the String.
+ * @param {Date} dt
+ * @returns {string} "YYYY-MM-DD"
+ */
+export const datetimeToString = (dt) => {
+    return dt.getFullYear() + '-'
+        + ('00' + (dt.getMonth()+1)).slice(-2) + '-'
+        + ('00' + dt.getDate()).slice(-2) + ' '
+        + ('00' + dt.getHours()).slice(-2) + ':'
+        + ('00' + dt.getMinutes()).slice(-2) + ':'
+        + ('00' + dt.getSeconds()).slice(-2)
+};
