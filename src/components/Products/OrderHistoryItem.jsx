29c46f24e478e4fce3ee33734c01b82b53c2f6a5
shtwangy <shtwangy@gmail.com>
2020-10-05 23:29:17 +0900

    feat(src/components/Products/OrderHistoryItem.jsx): display ordered products.
    Closes #9

--- src/components/Products/OrderHistoryItem.jsx
+++ src/components/Products/OrderHistoryItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Divider from "@material-ui/core/Divider";
 import {TextDetail} from "../UIkit";
 import {dateToString, datetimeToString} from "../../functions/common";
+import {OrderedProducts} from "./index";
 
 const OrderHistoryItem = (props) => {
     const order = props.order;
@@ -15,6 +16,12 @@ const OrderHistoryItem = (props) => {
             <TextDetail label={'注文日時'} value={orderedDate} />
             <TextDetail label={'発送予定日'} value={shippingDate} />
             <TextDetail label={'注文金額'} value={price} />
+            {order.products.length > 0 && (
+                <OrderedProducts
+                    key={order.id}
+                    products={order.products}
+                />
+            )}
             <div className='module-spacer--extra-extra-small' />
             <Divider />
         </div>
