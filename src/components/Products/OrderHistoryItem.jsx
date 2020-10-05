86d36a4330e064b05fa563573f3f274351bf2576
shtwangy <shtwangy@gmail.com>
2020-10-05 22:20:15 +0900

    feat(src/components/Products): create order history item component.
    Issues #9

---
+++ src/components/Products/OrderHistoryItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Divider from "@material-ui/core/Divider";
+import {TextDetail} from "../UIkit";
+import {dateToString, datetimeToString} from "../../functions/common";
+
+const OrderHistoryItem = (props) => {
+    const order = props.order;
+    const orderedDate = datetimeToString(order.updated_at.toDate());
+    const shippingDate = dateToString(order.shippingDate.toDate());
+    const price = '¥' + order.amount.toLocaleString();
+    return (
+        <div>
+            <div className='module-spacer--small' />
+            <TextDetail label={'注文ID'} value={order.id} />
+            <TextDetail label={'注文日時'} value={orderedDate} />
+            <TextDetail label={'発送予定日'} value={shippingDate} />
+            <TextDetail label={'注文金額'} value={price} />
+            <div className='module-spacer--extra-extra-small' />
+            <Divider />
+        </div>
+    );
+};
+
+export default OrderHistoryItem;
