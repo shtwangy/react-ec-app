2de375ed0f97804bb98ecac4e9baf421c95b50a8
shtwangy <shtwangy@gmail.com>
2020-11-16 16:06:33 +0900

    feat(src/reducks/users/reducers.js): add fetch favorite products reducer.
    Issues #24

--- src/reducks/users/reducers.js
+++ src/reducks/users/reducers.js
@@ -3,6 +3,11 @@ import initialState from "../store/initialState";
 
 export const UsersReducer= (state = initialState.users, action) => {
     switch (action.type) {
+        case Actions.FETCH_FAVORITE_PRODUCTS:
+            return {
+                ...state,
+                favorite: [...action.payload]
+            }
         case Actions.FETCH_ORDERS_HISTORY:
             return {
                 ...state,
