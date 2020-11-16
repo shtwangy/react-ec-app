b033471de4258e6b53d531b2eca3ed2a58198032
shtwangy <shtwangy@gmail.com>
2020-11-16 16:08:08 +0900

    feat(src/Router.jsx): add favorite list routing.
    Issues #24

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { SignUp, SignIn, Reset, ProductEdit, ProductList, ProductDetail, CartList, OrderConfirm, OrderHistory } from "./templates";
+import { SignUp, SignIn, Reset, ProductEdit, ProductList, ProductDetail, CartList, OrderConfirm, OrderHistory, FavoriteList } from "./templates";
 import Auth from "./Auth";
 
 const Router = () => {
@@ -15,6 +15,7 @@ const Router = () => {
                 <Route exact path="/product/:id" component={ProductDetail}/>
                 <Route path="/product/edit(/:id)?" component={ProductEdit}/>
                 <Route exact path="/cart" component={CartList}/>
+                <Route exact path="/favorite" component={FavoriteList}/>
                 <Route exact path="/order/confirm" component={OrderConfirm}/>
                 <Route exact path="/order/history" component={OrderHistory}/>
             </Auth>
